5f3217870bbec16b9a3b3522681805a8da36b1c1
Tim Langlois <langlois@cs.cornell.edu>
2013-08-05 09:45:04 -0400

    do not use js for image, but save function in case I decide to do it again

---
+++ assets/js/getImg.js
@@ -0,0 +1,13 @@
+<script>
+    function setUpImage()
+    {
+        var img = document.getElementById('myImg');
+
+        img.onload = function () {
+            //document.getElementById('contact-table-div').style.width = img.clientWidth.toString() + "px"
+        }
+
+        img.src = 'assets/img/me.jpg'
+    }
+</script>
+
